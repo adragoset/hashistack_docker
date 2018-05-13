c5a6b7c3a3bfb67fbb857b9c1f895d04598e4f0c
Alexander Dragoset <a.dragoset@gmail.com>
2018-05-13 01:19:32 -0500

    copy hashi implementation of container.

--- nomad/arm/docker-entrypoint.sh
+++ nomad/arm/docker-entrypoint.sh
@@ -1,13 +1,41 @@
-#!/usr/local/bin/dumb-init /bin/sh
-# Script created following Hashicorp's model for Consul:
-# https://github.com/hashicorp/docker-consul/blob/master/0.X/docker-entrypoint.sh
-# Comments in this file originate from the project above, simply replacing 'Consul' with 'Nomad'.
+#!/usr/bin/dumb-init /bin/sh
 set -e
 
 # Note above that we run dumb-init as PID 1 in order to reap zombie processes
 # as well as forward signals to all processes in its session. Normally, sh
 # wouldn't do either of these functions so we'd leak zombies as well as do
 # unclean termination of all our sub-processes.
+# As of docker 1.13, using docker run --init achieves the same outcome.
+
+# You can set NOMAD_BIND_INTERFACE to the name of the interface you'd like to
+# bind to and this will look up the IP and pass the proper -bind= option along
+# to Nomad.
+NOMAD_BIND=
+if [ -n "$NOMAD_BIND_INTERFACE" ]; then
+  NOMAD_BIND_ADDRESS=$(ip -o -4 addr list $NOMAD_BIND_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1)
+  if [ -z "$NOMAD_BIND_ADDRESS" ]; then
+    echo "Could not find IP for interface '$NOMAD_BIND_INTERFACE', exiting"
+    exit 1
+  fi
+
+  NOMAD_BIND="-bind=$NOMAD_BIND_ADDRESS"
+  echo "==> Found address '$NOMAD_BIND_ADDRESS' for interface '$NOMAD_BIND_INTERFACE', setting bind option..."
+fi
+
+# You can set NOMAD_CLIENT_INTERFACE to the name of the interface you'd like to
+# bind client intefaces (HTTP, DNS, and RPC) to and this will look up the IP and
+# pass the proper -client= option along to Nomad.
+NOMAD_CLIENT=
+if [ -n "$NOMAD_CLIENT_INTERFACE" ]; then
+  NOMAD_CLIENT_ADDRESS=$(ip -o -4 addr list $NOMAD_CLIENT_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1)
+  if [ -z "$NOMAD_CLIENT_ADDRESS" ]; then
+    echo "Could not find IP for interface '$NOMAD_CLIENT_INTERFACE', exiting"
+    exit 1
+  fi
+
+  NOMAD_CLIENT="-client=$NOMAD_CLIENT_ADDRESS"
+  echo "==> Found address '$NOMAD_CLIENT_ADDRESS' for interface '$NOMAD_CLIENT_INTERFACE', setting client option..."
+fi
 
 # NOMAD_DATA_DIR is exposed as a volume for possible persistent storage. The
 # NOMAD_CONFIG_DIR isn't exposed as a volume but you can compose additional
@@ -15,13 +43,6 @@ set -e
 # below.
 NOMAD_DATA_DIR=/nomad/data
 NOMAD_CONFIG_DIR=/nomad/config
-
-if [ -S '/var/run/docker.sock' ]; then
-    DOCKER_GID=$(stat -c '%g' '/var/run/docker.sock')
-    echo "docker group: ${DOCKER_GID}"
-    groupadd -for -g ${DOCKER_GID} hostdocker
-    usermod -aG hostdocker nomad
-fi
 
 # You can also set the NOMAD_LOCAL_CONFIG environemnt variable to pass some
 # Nomad configuration JSON without having to bind any volumes.
@@ -40,7 +61,9 @@ if [ "$1" = 'agent' ]; then
     shift
     set -- nomad agent \
         -data-dir="$NOMAD_DATA_DIR" \
-        -config="$NOMAD_CONFIG_DIR" \
+        -config-dir="$NOMAD_CONFIG_DIR" \
+        $NOMAD_BIND \
+        $NOMAD_CLIENT \
         "$@"
 elif [ "$1" = 'version' ]; then
     # This needs a special case because there's no help output.
@@ -53,27 +76,16 @@ fi
 
 # If we are running Nomad, make sure it executes as the proper user.
 if [ "$1" = 'nomad' ]; then
-    
     # If the data or config dirs are bind mounted then chown them.
     # Note: This checks for root ownership as that's the most common case.
-    if [ "$(stat -c %u /nomad/config)" != "$(id -u root)" ]; then
-        chown root:root /nomad/config
+    if [ "$(stat -c %u /nomad/data)" != "$(id -u nomad)" ]; then
+        chown nomad:nomad /nomad/data
+    fi
+    if [ "$(stat -c %u /nomad/config)" != "$(id -u nomad)" ]; then
+        chown nomad:nomad /nomad/config
     fi
 
-    if [ "$(stat -c %u /nomad/data)" != "$(id -u root)" ]; then
-        chown root:root /nomad/data
-    fi
-
-    update-ca-certificates /nomad/config/ssl/cluster-ca.pem
-
-    # If requested, set the capability to bind to privileged ports before
-    # we drop to the non-root user. Note that this doesn't work with all
-    # storage drivers (it won't work with AUFS).
-    if [ ! -z ${NOMAD+x} ]; then
-        setcap "cap_net_bind_service=+ep" /bin/nomad
-    fi
-
-    set -- gosu root "$@"
+    set -- su-exec nomad:nomad "$@"
 fi
 
 exec "$@"
