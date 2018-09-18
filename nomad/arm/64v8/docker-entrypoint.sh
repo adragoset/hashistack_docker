c7c7b4776534c26b8e12cac508e9ce40f346dbc7
PROD\adragose <Alexander.Dragoset@Intralox.com>
2018-09-18 14:41:41 -0500

    chown recirsively bind mounted folders.

--- nomad/arm/64v8/docker-entrypoint.sh
+++ nomad/arm/64v8/docker-entrypoint.sh
@@ -79,10 +79,10 @@ if [ "$1" = 'nomad'  ] || ; then
     # If the data or config dirs are bind mounted then chown them.
     # Note: This checks for root ownership as that's the most common case.
     if [ "$(stat -c %u /nomad/data)" != "$(id -u nomad)" ]; then
-        chown -r nomad:nomad /nomad/data
+        chown -R nomad:nomad /nomad/data
     fi
     if [ "$(stat -c %u /nomad/config)" != "$(id -u nomad)" ]; then
-        chown -r nomad:nomad /nomad/config
+        chown -R nomad:nomad /nomad/config
     fi
 
     set -- su-exec nomad:nomad "$@"
