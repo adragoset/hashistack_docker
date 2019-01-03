e511a6a78423af6a106437e30af37f294e7d367c
PROD\adragose <Alexander.Dragoset@Intralox.com>
2019-01-03 09:56:57 -0600

    Update consul

--- build_arm64.sh
+++ build_arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build -t devopsintralox/nomad:arm_v0.8.6 --network=cluster_network -f nomad/arm/64v8/Dockerfile .
-docker build -t devopsintralox/consul:arm_v1.2.3 --network=cluster_network -f consul/arm/64v8/Dockerfile .
+docker build -t devopsintralox/nomad:arm64v8_v0.8.6 --network=cluster_network -f nomad/arm/64v8/Dockerfile .
+docker build -t devopsintralox/consul:arm64v8_v1.4.0 --network=cluster_network -f consul/arm/64v8/Dockerfile .
 
-docker push devopsintralox/nomad:arm_v0.8.6
-docker push devopsintralox/consul:arm_v1.2.3
+docker push devopsintralox/nomad:arm64v8_v0.8.6
+docker push devopsintralox/consul:arm64v8_v1.4.0
