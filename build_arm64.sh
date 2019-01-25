de95f611010abb7bcd8a641224593078a43e29ab
Alex Dragoset <adragoset@users.noreply.github.com>
2019-01-25 15:28:19 -0600

    Update build_arm64.sh
    Bump versions

--- build_arm64.sh
+++ build_arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build -t devopsintralox/nomad:v0.8.6-arm64v8 --network=cluster_network -f nomad/arm/64v8/Dockerfile .
-docker build -t devopsintralox/consul:v1.4.0-arm64v8 --network=cluster_network -f consul/arm/64v8/Dockerfile .
+docker build -t devopsintralox/nomad:v0.8.7-arm64v8 --network=cluster_network -f nomad/arm/64v8/Dockerfile .
+docker build -t devopsintralox/consul:v1.4.1-arm64v8 --network=cluster_network -f consul/arm/64v8/Dockerfile .
 
-docker push devopsintralox/nomad:v0.8.6-arm64v8
-docker push devopsintralox/consul:v1.4.0-arm64v8
+docker push devopsintralox/nomad:v0.8.7-arm64v8
+docker push devopsintralox/consul:v1.4.1-arm64v8
