8690f5fc52e6ecbadc69614eda6928203bb37bc0
PROD\adragose <Alexander.Dragoset@Intralox.com>
2019-04-24 09:04:22 -0500

    bump consul 1.4.4

--- build_arm64.sh
+++ build_arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker build -t devopsintralox/nomad:v0.9.0-arm64v8 --network=cluster_network -f nomad/arm/64v8/Dockerfile .
-docker build -t devopsintralox/consul:v1.4.3-arm64v8 --network=cluster_network -f consul/arm/64v8/Dockerfile .
+docker build -t devopsintralox/consul:v1.4.4-arm64v8 --network=cluster_network -f consul/arm/64v8/Dockerfile .
 
 docker push devopsintralox/nomad:v0.9.0-arm64v8
-docker push devopsintralox/consul:v1.4.3-arm64v8
+docker push devopsintralox/consul:v1.4.4-arm64v8
