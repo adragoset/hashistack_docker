f7369f870bad333e01be980fa42dad9f1911cff5
PROD\adragose <Alexander.Dragoset@Intralox.com>
2019-05-10 10:39:55 -0500

    attempte to build an offline version of the nomad container.

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 docker build -t devopsintralox/nomad:v0.9.1 --network=cluster_network -f nomad/x64/Dockerfile .
 docker push devopsintralox/nomad:v0.9.1
+docker build -t devopsintralox/nomad:offline_v0.9.1 --network=cluster_network -f nomad/x64/offline/Dockerfile .
+docker push devopsintralox/nomad:offline_v0.9.1
