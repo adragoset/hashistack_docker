0bc0cf13a46e0b2de236d3c998d2ade9cfcb0de7
PROD\adragose <Alexander.Dragoset@Intralox.com>
2019-07-09 11:57:11 -0500

    build nomad 0.9.3

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-docker build -t devopsintralox/nomad:v0.9.1 --network=cluster_network -f nomad/x64/Dockerfile .
-docker push devopsintralox/nomad:v0.9.1
-docker build -t devopsintralox/nomad:offline-v0.9.1 --network=cluster_network -f nomad/x64/offline/Dockerfile .
-docker push devopsintralox/nomad:offline-v0.9.1
+docker build -t devopsintralox/nomad:v0.9.3 --network=cluster_network -f nomad/x64/Dockerfile .
+docker push devopsintralox/nomad:v0.9.3
--- build_arm64.sh
+++ build_arm64.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-docker build -t devopsintralox/nomad:v0.9.1-arm64v8 --network=cluster_network -f nomad/arm/64v8/Dockerfile .
-docker push devopsintralox/nomad:v0.9.1-arm64v8
-docker build -t devopsintralox/nomad:offline-v0.9.1-arm64v8 --network=cluster_network -f nomad/arm/64v8/offline/Dockerfile .
-docker push devopsintralox/nomad:offline-v0.9.1-arm64v8
+docker build -t devopsintralox/nomad:v0.9.3-arm64v8 --network=cluster_network -f nomad/arm/64v8/Dockerfile .
+docker push devopsintralox/nomad:v0.9.3-arm64v8
 docker build -t devopsintralox/consul:v1.4.4-arm64v8 --network=cluster_network -f consul/arm/64v8/Dockerfile .
 docker push devopsintralox/consul:v1.4.4-arm64v8
