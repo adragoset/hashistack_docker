727e10860ccb3ca0d266bc5058bb9ba76e7986ff
PROD\adragose <Alexander.Dragoset@Intralox.com>
2020-06-02 11:25:16 -0500

    work on setting up a gpu based container for nomad.

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-docker build -t devopsintralox/nomad:v0.10.4 --network=cluster_network -f nomad/x64/Dockerfile .
+docker build -t devopsintralox/nomad:v0.10.4 --network=cluster_network -f nomad/x64/cpu/Dockerfile .
 docker push devopsintralox/nomad:v0.10.4
+
+docker build -t devopsintralox/nomad:v0.10.4-gpu --network=cluster_network -f nomad/x64/gpu/Dockerfile .
+docker push devopsintralox/nomad:v0.10.4-gpu
