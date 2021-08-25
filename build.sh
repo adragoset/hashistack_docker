aebe0b82225bcca85f8074fe407fe9f6871ddaee
alievrouw <adamlievrouw@gmail.com>
2021-08-25 14:41:57 -0500

    Bumping Nomad version to 1.1.3

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build -t devopsintralox/nomad:v0.10.4 --network=cluster_network -f nomad/x64/cpu/Dockerfile .
-docker push devopsintralox/nomad:v0.10.4
+docker build -t devopsintralox/nomad:v1.1.3 --network=cluster_network -f nomad/x64/cpu/Dockerfile .
+docker push devopsintralox/nomad:v1.1.3
 
-docker build -t devopsintralox/nomad:v0.10.4-gpu --network=cluster_network -f nomad/x64/gpu/Dockerfile .
-docker push devopsintralox/nomad:v0.10.4-gpu
+docker build -t devopsintralox/nomad:v1.1.3-gpu --network=cluster_network -f nomad/x64/gpu/Dockerfile .
+docker push devopsintralox/nomad:v1.1.3-gpu
