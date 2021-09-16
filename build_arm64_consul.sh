26d0ed0db51f0403cb22b3a52aff5b424a33e20d
Alex Dragoset <adragoset@users.noreply.github.com>
2021-09-16 13:19:01 -0500

    Update build_arm64_consul.sh
    bump consul version

--- build_arm64_consul.sh
+++ build_arm64_consul.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t devopsintralox/consul:v1.6.4-arm64v8 --network=cluster_network -f consul/arm/64v8/Dockerfile .
-docker push devopsintralox/consul:v1.6.4-arm64v8
+docker build -t devopsintralox/consul:v1.10.2-arm64v8 --network=cluster_network -f consul/arm/64v8/Dockerfile .
+docker push devopsintralox/consul:v1.10.2-arm64v8
