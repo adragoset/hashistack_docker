bc81db01dadffb31610540e643b4a03603b04e6f
Alex Dragoset <adragoset@users.noreply.github.com>
2023-06-05 16:48:45 -0500

    Update build.sh
    Update the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build -t devopsintralox/nomad:v1.1.6 --network=cluster_network -f nomad/x64/cpu/Dockerfile .
-docker push devopsintralox/nomad:v1.1.6
+docker build -t devopsintralox/nomad:v1.5.3 --network=cluster_network -f nomad/x64/cpu/Dockerfile .
+docker push devopsintralox/nomad:v1.5.3
 
 docker build -t devopsintralox/nomad:v1.1.6-gpu --network=cluster_network -f nomad/x64/gpu/Dockerfile .
 docker push devopsintralox/nomad:v1.1.6-gpu
